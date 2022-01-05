66a6a23ac063927f2b421b70b25a1a80b1d03fd5
fvantriel <florian.vantriel@gmail.com>
2022-01-05 16:39:56 +0100

    Add firebase integration and migrate android manifest + updating url launcher dependency to resolve build conflicts with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_resume_website/UI/Home.dart';
 import 'Model/WebPageTransitionDisabler.dart';
+import 'package:flutter_resume_website/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
+//
+// void main() {
+//   runApp(MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   @override
