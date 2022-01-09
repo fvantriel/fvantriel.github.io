46490b4af42e1289aa0f449430d947443e0f2712
fvantriel <florian.vantriel@gmail.com>
2022-01-09 19:24:12 +0100

    Tailor mobile page style

--- lib/Data/StyleConstants.dart
+++ lib/Data/StyleConstants.dart
@@ -4,3 +4,5 @@ const Color kBackgroundColor = Color(0xff0A192F);
 
 const Color kIconColor = Color(0xffffA8B2D1);
 const double kIconSize = 20.0;
+
+const double kMobileDescFontSize = 14.0;
--- lib/UI/AboutMobile.dart
+++ lib/UI/AboutMobile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_resume_website/Widget/CustomText.dart';
 import 'package:flutter_resume_website/Model/Method.dart';
+import 'package:flutter_resume_website/Data/StyleConstants.dart';
 
 class AboutMobile extends StatelessWidget {
   Widget technology(BuildContext context, String text) {
@@ -41,14 +42,14 @@ class AboutMobile extends StatelessWidget {
               color: Color(0xff61F9D5),
               fontWeight: FontWeight.w700,
             ),
-            SizedBox(width: size.width * 0.001),
+            SizedBox(width: size.width * 0.02),
             CustomText(
               text: "About Me",
               textsize: method.adaptFontSizeToScreenSize(26.0, size.width),
               color: Color(0xffCCD6F6),
               fontWeight: FontWeight.w700,
             ),
-            SizedBox(width: size.width * 0.001),
+            SizedBox(width: size.width * 0.02),
             Container(
               width: size.width / 4,
               height: size.height * 0.001,
@@ -56,40 +57,12 @@ class AboutMobile extends StatelessWidget {
             ),
           ],
         ),
-        SizedBox(height: size.height * 0.07),
+        SizedBox(height: size.height * 0.01),
         Container(
-          height: size.height / 1.5,
-          width: size.width * 0.9 / 2,
-          //color: Colors.white, //used for debugging
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                top: size.height * 0.04,
-                left: size.width * 0.120,
-                child: Card(
-                  color: Color(0xff61F9D5),
-                  child: Container(
-                    margin: EdgeInsets.all(2.75),
-                    height: size.height / 2,
-                    width: size.width / 5,
-                    color: Color(0xff0A192F),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: size.height * 0.01,
-                child: Container(
-                  height: size.height / 2,
-                  width: size.width / 5,
-                  color: Colors.black54,
-                  child: Image(
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/images/fvan.jpeg"),
-                  ),
-                ),
-              ),
-            ],
+          height: size.height / 4,
+          width: size.width / 4,
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/images/fvan.jpeg"),
           ),
         ),
         SizedBox(height: size.height * 0.01),
@@ -99,20 +72,20 @@ class AboutMobile extends StatelessWidget {
             CustomText(
               text:
                   "Hi! I am Florian, an engineer based in Munich, Germany.\n\nI am passionate about the latest technology and aim to build products with focus on the users. In my current role I am responsible for security functionalities of central vehicle components and functions of all BMW, Mini and Rolls Royce vehicles on the road.\nAdditionally, I am the go-to person regarding information protection and secure data handling in my department.\n\n",
-              textsize: method.adaptFontSizeToScreenSize(16.0, size.width),
+              textsize: kMobileDescFontSize,
               color: Color(0xff828DAA),
               letterSpacing: 0.75,
             ),
             CustomText(
               text:
                   "I have a master's degree in engineering from the Technical University of Dresden and wrote my thesis in a collaborative research project with the University of California at Berkeley.\n\n",
-              textsize: method.adaptFontSizeToScreenSize(16.0, size.width),
+              textsize: kMobileDescFontSize,
               color: Color(0xff828DAA),
             ),
             CustomText(
               text:
                   "Here are a few technologies I have been working with so far:\n\n",
-              textsize: method.adaptFontSizeToScreenSize(16.0, size.width),
+              textsize: kMobileDescFontSize,
               color: Color(0xff828DAA),
               // fontWeight: FontWeight.w500,
               letterSpacing: 0.75,
