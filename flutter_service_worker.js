386d38a2101cd445fb2dab335ba97caa1c147b77
fvantriel <florian.vantriel@gmail.com>
2022-01-18 21:35:22 +0100

    Built web
    Branch: main
    Commit: 1648e0cfef4bd8005044e456c16c17b628f9d2d4 (dirty)
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "7d7b8aa95d63e153e3d54d4b77729340",
-"index.html": "d7a82a02425f450aabfc5f9446ced39b",
-"/": "d7a82a02425f450aabfc5f9446ced39b",
-"main.dart.js": "e93a608d65fc79b68ae40d5a18e246a2",
+"index.html": "1fa3c5e68cefe5238bd006b735b88ed0",
+"/": "1fa3c5e68cefe5238bd006b735b88ed0",
+"main.dart.js": "5927afa9c49575737b50238a94b0b034",
 "favicon.png": "cec06261144dd5f33864440510a04c82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
