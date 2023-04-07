a7a77e443855692b0fe8e08b1cb7619f74b6705b
fvantriel <florian.vantriel@gmail.com>
2023-04-07 19:31:19 +0200

    $commitMessage

---
+++ push_code_and_website.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This bash script is being used to upload the latest repo state to git and push the changes of the website to gh-pages
+echo "Hi there, let's upload the latest state of the repository!"
+
+echo ""
+
+git add .
+
+echo "Please enter commit message"
+read commitMessage
+
+echo $commitMessage
+
+git commit -m '$commitMessage'
+#
+#git push origin
+#
+#echo "Now that your codebase is updated, let's push the changes to your website!"
+#
+#flutter pub global activate peanut
+#
+#flutter pub global run peanut
+#
+#git push origin --set-upstream gh-pages
+#
+#echo ""
+#echo ""
+#echo "Done, all changes should be up to date in the next minutes."
+#
